7ab94b0803589b3e53231a3f3a3162d6777007a2
Mary <mvjiang@gmail.com>
2019-08-17 09:50:36 -0600

    Almost done OIT

--- pages/project.js
+++ pages/project.js
@@ -5,8 +5,30 @@
   $(document).ready(function() {
     AOS.init();
 
+    let mainNavLinks = document.querySelectorAll(".sideNav ul li a");
+    let mainSections = document.querySelectorAll("body section");
 
-console.log("hi")
+    console.log(mainSections);
+
+    let lastId;
+    let cur = [];
+
+    window.addEventListener("scroll", event => {
+      let fromTop = window.scrollY;
+
+      mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (
+          section.offsetTop <= fromTop &&
+          section.offsetTop + section.offsetHeight > fromTop
+        ) {
+          link.classList.add("current");
+        } else {
+          link.classList.remove("current");
+        }
+      });
+    });
 
 
   })
