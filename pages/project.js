254536fddff42d9ed0a37e34977ae1dcb2c82ab6
Mary Jiang <mvj6@CDSS-4202.local>
2019-09-22 13:21:04 -0400

    changed heading formatting for projects

--- pages/project.js
+++ pages/project.js
@@ -1,35 +1,57 @@
 
 
 
-
-  $(document).ready(function() {
+$(document).ready(function() {
     AOS.init();
+    $(document).on("scroll", onScroll);
 
-    let mainNavLinks = document.querySelectorAll(".sideNav ul li a");
-    let mainSections = document.querySelectorAll("body section");
-
-    console.log(mainSections);
-
-    let lastId;
-    let cur = [];
-
-    window.addEventListener("scroll", event => {
-      let fromTop = window.scrollY;
-
-      mainNavLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
-
-        if (
-          section.offsetTop <= fromTop &&
-          section.offsetTop + section.offsetHeight > fromTop
-        ) {
-          link.classList.add("current");
-        } else {
-          link.classList.remove("current");
-        }
-      });
+    $(document).scroll(function() {
+      var scrollPos = $(document).scrollTop();
+      if (scrollPos > $('.hero').height()) {
+        $('.sideNav').fadeIn(500);
+      } else {
+        $('.sideNav').fadeOut(200);
+      }
     });
 
 
-  })
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
 
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+
+    $('.sideNav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          console.log(currLink)
+            $('.sideNav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+    }
+
+
+});
