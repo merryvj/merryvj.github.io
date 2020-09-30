30df6e07a0e519340e54b578b36f612168d2dc50
Mary Jiang <mvj6@duke.edu>
2020-09-29 20:13:34 -0600

    first push of 2020 content

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
+[].forEach.call(document.getElementsByClassName("iframe-lightbox-link"), function (el) {
+    el.lightbox = new IframeLightbox(el);
+  });
 
 
+  jQuery(function ($) {
+    $.fn.hScroll = function (amount) {
+        amount = amount || 120;
+        $(this).bind("DOMMouseScroll mousewheel", function (event) {
+            var oEvent = event.originalEvent, 
+                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
+                position = $(this).scrollLeft();
+            position += direction > 0 ? -amount : amount;
+            $(this).scrollLeft(position);
+            event.preventDefault();
+        })
+    };
+});
 
-
-  $(document).ready(function() {
-    AOS.init();
-
-
-console.log("hi")
-
-    var options = {
-        strings: ["<span style='color: #5FB7DD'> design</span>.", "<span style='color: #FFC280'> develop</span>.", "<span style='color: #4AD4B3'> dream</span>.", "<span style='color: #D0B3FF'> dance</span>."],
-        typeSpeed: 50,
-        backSpeed: 40,
-        showCursor: false,
-        loop: true
-      }
-      
-      var typed = new Typed("#typedStrings", options);
-
-
-
-  })
-
+$(document).ready(function() {
+    $('.wrapper').hScroll(60); // You can pass (optionally) scrolling amount
+});
