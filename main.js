6936c3dd049a09dca3f3e8a26e32fd445a92f1c3
Mary Jiang <mvj6@duke.edu>
2020-10-21 00:33:13 -0600

    finished vertical styling

--- main.js
+++ main.js
@@ -2,21 +2,3 @@
     el.lightbox = new IframeLightbox(el);
   });
 
-
-  jQuery(function ($) {
-    $.fn.hScroll = function (amount) {
-        amount = amount || 120;
-        $(this).bind("DOMMouseScroll mousewheel", function (event) {
-            var oEvent = event.originalEvent, 
-                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
-                position = $(this).scrollLeft();
-            position += direction > 0 ? -amount : amount;
-            $(this).scrollLeft(position);
-            event.preventDefault();
-        })
-    };
-});
-
-$(document).ready(function() {
-    $('.wrapper').hScroll(40); // You can pass (optionally) scrolling amount
-});
